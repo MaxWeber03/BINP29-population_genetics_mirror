97d30322a1b14fc450546f45ba92d20a326847ea
MaxWeber03 <maxweber@uni-bremen.de>
2026-03-10 17:13:41 +0100

    add metaphlan download

---
+++ 06_metaphlan.sh
@@ -0,0 +1,9 @@
+# Install metaphlan
+
+conda create -n metaphlan -c bioconda
+conda activate metaphlan
+conda install bioconda::metaphlan
+
+# Verify Version
+metaphlan --version
+# MetaPhlAn version 4.2.4 (21 Oct 2025)
