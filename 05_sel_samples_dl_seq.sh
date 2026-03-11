d93292944c3eb3e0e5d5a4d98495b8c2ef943317
MaxWeber03 <maxweber@uni-bremen.de>
2026-03-11 11:31:09 +0100

    fix broken filenames in fastq download, add multithread unzipping of sequences

--- 05_sel_samples_dl_seq.sh
+++ 05_sel_samples_dl_seq.sh
@@ -8,20 +8,20 @@ mkdir 07_sample_seq
 # to download the sequences, we can loop over the list of samples, then extract the link from the 
 # metadata table with awk, extract the filename from link, and download the file with curl
 # This is done twice, once for each link (fwd/rev).
+# The naming of files assumes that the files are saved as *.fastq.gz on the server
 cat 02_sample_list/sample_selection_for_analysis.txt | \
     while read line; do
         link1=$(grep $line 05_metadata_cleaned/metadata_cleaned.tsv | awk {'print $2'})
-        filename1=$(basename "$link1")
-        curl -o "07_sample_seq/$filename1" "$link1"
+        curl -o "07_sample_seq/${line}_1.fastq.gz" "$link1"
 
         link2=$(grep $line 05_metadata_cleaned/metadata_cleaned.tsv | awk {'print $3'})
-        filename2=$(basename "$link2")
-        curl -o "07_sample_seq/$filename2" "$link2"
+        curl -o "07_sample_seq/${line}_2.fastq.gz" "$link2"
 done
 
 echo "Download of sequences finished"
 
-gunzip 07_sample_seq/*.gz
+# decompress using up to 8 parallel processes (8 threads)
+ls 07_sample_seq/*.gz | xargs -n 1 -P 8 gunzip
 
 echo "Decompression finished"
 
