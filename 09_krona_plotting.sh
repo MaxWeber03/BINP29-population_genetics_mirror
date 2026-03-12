78bc15ebf881149a0c59454624c08f9d0c95f2ca
MaxWeber03 <maxweber@uni-bremen.de>
2026-03-12 09:16:36 +0100

    add script for basic krona plot

---
+++ 09_krona_plotting.sh
@@ -0,0 +1,26 @@
+# THis script takes the files from 12_data_for_krona/ and creates krona plots from them.
+
+# Install krona with conda
+# conda create -n krona -c bioconda
+conda activate krona
+# conda install krona=2.8.1
+
+# Check version
+ktImportText
+# KronaTools 2.8.1 - ktImportText
+
+mkdir 13_krona_output
+
+# run ktImportText
+for sample in 12_data_for_krona/*.tsv
+
+    # extract sample name
+    sample_name=$(basename "$sample" .tsv)
+    
+    # run ktImportText to create Krona plots
+    ktImportText \
+        -o  13_krona_output/$sample.html \
+        -n 'all' \
+        $sample
+        
+done
