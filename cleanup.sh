320de2d9fbd51b4970d9b1cffc977234c54f4088
MaxWeber03 <maxweber@uni-bremen.de>
2026-03-13 11:25:29 +0100

    add script to delete all intermediate files to save disk space

---
+++ cleanup.sh
@@ -0,0 +1,11 @@
+# WARNING: THIS SCRIPT DELETES ALL INTERMEDIATE FILES THAT ARE NOT THE FINAL RESULTS OR THE INPUT FILES
+
+rm -r 03_metadata/
+rm -r 04_metadata_table/
+rm -r 05_metadata_cleaned/
+rm -r 07_sample_seq/
+rm -r 08_kraken_db/
+rm -r 09_kraken_output/
+rm -r 10_bracken_output/
+rm -r 11_taxonkit_db/
+rm -r 12_data_for_krona/
